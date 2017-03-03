ab15a872fd14c8dde560a4502358c08481c40c11
pashaUSA <pashausa@gmail.com>
2017-03-02 17:57:45 -0800

    Update variables.php

--- sections/functions/variables.php
+++ sections/functions/variables.php
@@ -5,5 +5,9 @@ $businessNameTags = '<strong>Nifty Tours</strong> traveling agency';
 $facebookLink = 'https://www.facebook.com/cas222cascade/';
 $twitterLink = 'https://twitter.com/pcccas222';
 
+$emailTo = 'pashaUSA@gmail.com';
+
+$emailFrom = 'Nifty\'s Travel<noreply@niftystours.com>';
+
 ?>
 
